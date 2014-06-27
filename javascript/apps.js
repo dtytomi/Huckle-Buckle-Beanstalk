51d91d35f0a77be2e7f5033a887889bbd18d81d0
unknown <Ayooluwa>
2014-06-27 10:22:52 +0100

    new updates for the apps.js file

--- javascript/apps.js
+++ javascript/apps.js
@@ -1,17 +1,33 @@
-var x = Math.ceil(Math.random() * 100);
+// to add a botton to the div tag
+$(".wrapper").append('<Input type="submit" value="Start">');
+
+// to select the button created
+$("input").click( function() {
+	var x = Math.ceil(Math.random() * 100);
 
 var y = 0;
 
 console.log(x);
 
+y = prompt("Input a value");
+    alert("You are Hot");
+
+
 while( x != y) {
-    y = prompt("Input a value");
-    if( y < x){
+
+	 var y = prompt("Input a value");
+
+    if( y < x ){
         alert("You are cold");
-    } else if( y > x){
+    } else if( y > x ){
         alert("You are hot");
     }
+
+    
 }
 
+alert("Congratulations!!! You Won");
+
+});
 
  
